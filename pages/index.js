e26a1ffa97b0d7227f6d598936531bcb60230182
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-07-02 17:50:27 -0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -55,7 +55,7 @@ export default function Home() {
           <MicrophoneIcon className="h-5"/>
         </div>
 
-        <div className="flex fleex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0
+        <div className="flex flex-col w-1/2 space-y-2 justify-center mt-8 sm:space-y-0
         sm:flex-row sm:space-x-4">
           <button className="btn">Google Search</button>
           <button className="btn">i'm Felling Lucky</button>
