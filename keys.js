e29180f4c6a6397a14ea8bfa2d2019fc94f02d61
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-07-03 15:08:14 -0300

    finished search result page

--- keys.js
+++ keys.js
@@ -3,8 +3,8 @@
 //https://developers.google.com/custom-search/v1/introduction#identify_your_application_to_google_with_api_key
 //https://www.googleapis.com/customsearch/v1?[parameters]
 
-export const API_KEY = ''
+export const API_KEY = 'AIzaSyDZ9n2ElsoEJUhA1E278HiXQ-e9wQZHOPA'
 
 // Context key https://cse.google.com/cse/create/new
 
-export const CONTEXT_KEY = ''
+export const CONTEXT_KEY = 'c1ddcce05742e891c'
